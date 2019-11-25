b90ef0f0c9b4e5ff856c9ca064b944e3fbaef24f
Huage <2334788801@qq.com>
2019-11-26 00:26:07 +0800

    '第二次'

---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>     <!--不具名插槽    组件中插组件要用到插槽-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar"
+}
+</script>>
+
+<style>
+ #tab-bar {
+   display: flex;
+   background-color: #f6f6f6;
+
+   position: fixed;
+   left: 0;
+   right: 0;
+   bottom: 0;
+  /*x轴向右为正， y轴向下为正*/
+   box-shadow: 0px -2px 1px rgba(100,100,100,.1);    
+
+ }
+ .tab-bar-item {
+   flex: 1;
+   text-align: center;
+   height: 49px;
+ }
+ .tab-bar-item img {
+   width: 24px;
+   height: 24px;
+ }
+</style>
---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">            <!--最好用div 把插槽给包住 这样属性在div里就不会被替换了-->
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :class="{active: isActive}">
+      <slot name="item-text"></slot>
+    </div>
+    <!--<img src="../../assets/img/tabbar/home.svg">
+    <div>首页</div>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path:String
+  },
+  data()
+  {
+    return {
+      // isActive:true
+    }
+  },
+  methods: {
+    itemClick() {
+      // console.log(111);
+      this.$router.replace(this.path)
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1
+    }
+  }
+}
+</script>
+<style>
+ .tab-bar-item {
+   flex: 1;
+   text-align: center;
+   height: 49px;
+   font-size: 14px;
+ }
+ .tab-bar-item img {
+   width: 24px;
+   height: 24px;
+   margin-top: 3px;
+   vertical-align: middle;      //图片下面会多3px   清除
+   margin-bottom: 2px;
+ }
+ .active {
+   color: red;
+ }
+</style>
